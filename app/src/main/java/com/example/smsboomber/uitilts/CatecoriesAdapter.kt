278739c4521ecd1e62a65b3a273cf6b9a28efbdb
yengilmas <lbek458@gmail.com>
2024-03-13 20:25:52 +0500

    Ilovaga bazi o'zgartishlar kiritilyabdi

---
+++ app/src/main/java/com/example/smsboomber/uitilts/CatecoriesAdapter.kt
@@ -0,0 +1,66 @@
+package com.example.smsboomber.uitilts
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import androidx.recyclerview.widget.RecyclerView
+import com.example.smsboomber.R
+import com.example.smsboomber.databinding.HistoryItemLayoutBinding
+import com.example.smsboomber.model.Message
+
+
+class CatecoriesAdapter(
+    var items: ArrayList<Message>,
+    var listener: ItemSetOnClickListener,
+    var context: Context
+) :
+    RecyclerView.Adapter<CatecoriesAdapter.Holder>() {
+
+    interface ItemSetOnClickListener {
+        fun onClick(data: Message)
+    }
+
+    inner class Holder(var view: HistoryItemLayoutBinding) : RecyclerView.ViewHolder(view.root) {
+
+        fun bind(data: Message) {
+
+            view.apply {
+
+                this.tvIndex.text=data.messageIndex
+                this.tvPhone.text=data.number
+                this.tvSms.text=data.message
+
+
+            }
+
+        }
+
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+
+        val binding =
+            HistoryItemLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return Holder(
+            binding
+        )
+
+
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        val item = items[position]
+
+        holder.bind(item)
+        holder.itemView.setOnClickListener {
+            listener.onClick(item)
+        }
+
+    }
+
+    override fun getItemCount(): Int = items.count()
+
+}
